8085f5d7e10abba471d2f91d97b834f4834f64ea
BastianTeufel-GTR <b.teufel@gtr.de>
2026-04-22 09:15:39 +0200

    Quartz sync: Apr 22, 2026, 9:15 AM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { joinSegments, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -6,9 +6,15 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const logoLight = joinSegments(baseDir, "static/gtr-logo.svg")
+  const logoDark = joinSegments(baseDir, "static/gtr-logo-negativ.svg")
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <img src={logoLight} alt="" class="page-title-logo page-title-logo-light" />
+        <img src={logoDark} alt="" class="page-title-logo page-title-logo-dark" />
+        <span>{title}</span>
+      </a>
     </h2>
   )
 }
@@ -19,6 +25,20 @@ PageTitle.css = `
   margin: 0;
   font-family: var(--titleFont);
 }
+.page-title a {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5em;
+  text-decoration: none;
+}
+.page-title-logo {
+  height: 2.2em;
+  width: auto;
+  flex-shrink: 0;
+}
+.page-title-logo-dark { display: none; }
+:root[saved-theme="dark"] .page-title-logo-light { display: none; }
+:root[saved-theme="dark"] .page-title-logo-dark  { display: inline; }
 `
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
